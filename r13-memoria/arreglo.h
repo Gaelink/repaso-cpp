f9d05ad8caf947723fcdb202c4a53afb27c459d5
Gael Sánchez Carrillo <a01643494@tec.mx>
2026-09-06 22:11:24 -0600

    R13: Arreglo con regla de tres

---
+++ r13-memoria/arreglo.h
@@ -0,0 +1,37 @@
+#include <cstddef>
+
+class Arreglo {
+private:
+    int* datos;
+    std::size_t tam;
+
+public:
+    // Constructor normal
+    Arreglo(std::size_t tam) : tam(tam), datos(new int[tam]{}) {}
+
+    // 1. DESTRUCTOR
+    ~Arreglo() {
+        delete[] datos;
+    }
+
+    // 2. CONSTRUCTOR DE COPIA
+    Arreglo(const Arreglo& otro) : tam(otro.tam), datos(new int[otro.tam]) {
+        for (std::size_t i = 0; i < tam; ++i) {
+            datos[i] = otro.datos[i];
+        }
+    }
+
+    // 3. OPERADOR DE ASIGNACIÓN (operator=)
+    Arreglo& operator=(const Arreglo& otro) {
+        if (this != &otro) {        // Evita la autoasignación (a = a)
+            delete[] datos;         // Liberar memoria actual
+
+            tam = otro.tam;
+            datos = new int[tam];   // Reservar nueva memoria
+            for (std::size_t i = 0; i < tam; ++i) {
+                datos[i] = otro.datos[i]; // Copiar elementos
+            }
+        }
+        return *this;
+    }
+};
